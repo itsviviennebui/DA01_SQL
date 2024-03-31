e5c8de4a0234e213da93a3c2bd3f47d2c8700df4
itsviviennebui <165192694+itsviviennebui@users.noreply.github.com>
2024-03-31 22:08:44 +0700

    Update SESSION7_PRACTICE3.sql

--- SESSION7_PRACTICE3.sql
+++ SESSION7_PRACTICE3.sql
@@ -21,3 +21,23 @@ GROUP BY manufacturer
 ORDER BY SUM(total_sales) DESC, manufacturer;
 
 -- Exercise 4
+SELECT
+EXTRACT(month FROM submit_date) AS mth,
+product_id AS product,
+ROUND(AVG(stars), 2) AS avg_stars
+FROM reviews
+GROUP BY EXTRACT(month FROM submit_date), product_id
+ORDER BY month, product;
+
+-- Exercise 5
+SELECT
+sender_id,
+COUNT(message_id) AS message_count
+FROM messages
+WHERE EXTRACT(month FROM sent_date)=8
+AND EXTRACT(year FROM sent_date)=2022
+GROUP BY sender_id
+ORDER BY COUNT(message_id) DESC
+LIMIT 2;
+
+-- Exercise 6
