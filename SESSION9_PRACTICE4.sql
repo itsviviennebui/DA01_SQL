f3227d7519d30858da0813ed5544af354c6ec5ce
itsviviennebui <165192694+itsviviennebui@users.noreply.github.com>
2024-04-02 23:28:38 +0700

    Update SESSION9_PRACTICE4.sql

--- SESSION9_PRACTICE4.sql
+++ SESSION9_PRACTICE4.sql
@@ -17,3 +17,28 @@ END AS triangle
 FROM Triangle;
 
 -- Exercise 3
+SELECT
+ROUND((SUM(CASE 
+    WHEN call_category IS NULL OR case_id = 'n/a' THEN 1 ELSE 0
+    END)/COUNT(case_id)*100.0), 1) AS call_percentage
+FROM callers;
+
+-- Exercise 4
+SELECT name
+FROM Customer
+WHERE referee_id !=2 OR referee_id IS NULL;
+
+-- Exercise 5
+SELECT
+survived,
+SUM(CASE
+    WHEN pclass = 1 THEN 1 ELSE 0
+    END) AS first_class,
+SUM(CASE
+    WHEN pclass = 2 THEN 1 ELSE 0
+    END) AS second_class,
+SUM(CASE
+    WHEN pclass = 3 THEN 1 ELSE 0
+    END) AS third_class
+FROM titanic
+GROUP BY survived;
