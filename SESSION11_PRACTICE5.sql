d5a7a5c2fba61c942bc6ea6bf69bc8e8d828fb08
itsviviennebui <165192694+itsviviennebui@users.noreply.github.com>
2024-04-08 12:40:01 +0700

    Update SESSION11_PRACTICE5.sql

--- SESSION11_PRACTICE5.sql
+++ SESSION11_PRACTICE5.sql
@@ -1,1 +1,11 @@
+-- Exercise 1
+SELECT
+COUNTRY.Continent,
+FLOOR(AVG(CITY.Population)) AS avg_population
+FROM CITY
+INNER JOIN COUNTRY
+ON CITY.CountryCode = COUNTRY.Code
+GROUP BY COUNTRY.Continent;
+
+-- Exercise 2
 
