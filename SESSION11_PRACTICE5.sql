13ff0c75a1f2b79244657959dff6f316642efe57
itsviviennebui <165192694+itsviviennebui@users.noreply.github.com>
2024-04-14 21:52:00 +0700

    Update SESSION11_PRACTICE5.sql

--- SESSION11_PRACTICE5.sql
+++ SESSION11_PRACTICE5.sql
@@ -30,3 +30,23 @@ WHERE activities.activity_type IN ('open', 'send')
 GROUP BY age_breakdown.age_bucket;
 
 -- Exercise 4
+SELECT customer_contracts.customer_id
+FROM customer_contracts
+INNER JOIN products
+ON customer_contracts.product_id = products.product_id
+GROUP BY customer_contracts.customer_id
+HAVING COUNT(DISTINCT products.product_category) = (SELECT COUNT(DISTINCT products.product_category) FROM products)
+
+-- Exercise 5
+SELECT
+manager.employee_id,
+manager.name,
+COUNT(employee.employee_id) AS reports_count,
+ROUND(AVG(employee.age)) AS average_age
+FROM Employees manager, Employees employee
+WHERE manager.employee_id = employee.reports_to
+GROUP BY 1
+HAVING reports_count >0
+ORDER BY 1;
+
+-- Exercise 6
