83b566396c59a0c3b59bedd20242045d0eec8b46
itsviviennebui <165192694+itsviviennebui@users.noreply.github.com>
2024-04-18 21:49:18 +0700

    Update SESSION13_PRACTICE6.sql

--- SESSION13_PRACTICE6.sql
+++ SESSION13_PRACTICE6.sql
@@ -67,4 +67,12 @@ AND EXTRACT(YEAR FROM current_month.event_date) = 2022
 GROUP BY EXTRACT(MONTH FROM current_month.event_date);
 
 -- Exercise 6
-
+SELECT
+DATE_FORMAT(trans_date, '%Y-%m') AS month,
+country,
+COUNT(id) AS trans_count,
+SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+SUM(amount) AS trans_total_amount,
+SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY DATE_FORMAT(trans_date, '%Y-%m'), country;
