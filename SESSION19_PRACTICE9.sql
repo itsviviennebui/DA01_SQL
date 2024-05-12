acc9dc607a29546e0f6ed1fbb1a93fb988d92169
itsviviennebui <165192694+itsviviennebui@users.noreply.github.com>
2024-05-12 23:14:43 +0700

    Update SESSION19_PRACTICE9.sql

--- SESSION19_PRACTICE9.sql
+++ SESSION19_PRACTICE9.sql
@@ -1,1 +1,28 @@
 -- Q1: Chuyển đổi kiểu dữ liệu phù hợp cho các trường ( sử dụng câu lệnh ALTER) 
+
+
+
+
+
+
+ ordernumber VARCHAR > interger
+  quantityordered VARCHAR > integer
+  priceeach        VARCHAR > numeric
+  orderlinenumber  VARCHAR > numeric
+  sales            VARCHAR > numeric
+  orderdate        VARCHAR > datetime
+  status           VARCHAR 
+  productline      VARCHAR 
+  msrp             VARCHAR > integer
+  productcode      VARCHAR > integer
+  customername     VARCHAR 
+  phone            VARCHAR 
+  addressline1     VARCHAR,
+  addressline2     VARCHAR,
+  city             VARCHAR,
+  state            VARCHAR,
+  postalcode       VARCHAR
+  country          VARCHAR,
+  territory        VARCHAR,
+  contactfullname  VARCHAR,
+  dealsize         VARCHAR
